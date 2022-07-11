ca43f9280eb69ce5e4f4dfa75fac63a4f5380e07
Lennart Tönjes <toenjes@tremaze.de>
2022-07-11 14:47:32 +0200

    made interceptor optional

--- projects/lib/src/angular-oauth-oidc.module.ts
+++ projects/lib/src/angular-oauth-oidc.module.ts
@@ -1,56 +1,67 @@
-import { DateTimeProvider, SystemDateTimeProvider } from './date-time-provider';
-import { OAuthStorage, OAuthLogger } from './types';
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {DateTimeProvider, SystemDateTimeProvider} from './date-time-provider';
+import {OAuthLogger, OAuthStorage} from './types';
+import {ModuleWithProviders, NgModule, Provider} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { OAuthService } from './oauth-service';
-import { UrlHelperService } from './url-helper.service';
+import {OAuthService} from './oauth-service';
+import {UrlHelperService} from './url-helper.service';
 
-import { OAuthModuleConfig } from './oauth-module.config';
-import {
-  OAuthResourceServerErrorHandler,
-  OAuthNoopResourceServerErrorHandler,
-} from './interceptors/resource-server-error-handler';
-import { DefaultOAuthInterceptor } from './interceptors/default-oauth.interceptor';
-import { ValidationHandler } from './token-validation/validation-handler';
-import { NullValidationHandler } from './token-validation/null-validation-handler';
-import { createDefaultLogger, createDefaultStorage } from './factories';
-import {
-  HashHandler,
-  DefaultHashHandler,
-} from './token-validation/hash-handler';
+import {OAuthModuleConfig} from './oauth-module.config';
+import {OAuthNoopResourceServerErrorHandler, OAuthResourceServerErrorHandler} from './interceptors/resource-server-error-handler';
+import {DefaultOAuthInterceptor} from './interceptors/default-oauth.interceptor';
+import {ValidationHandler} from './token-validation/validation-handler';
+import {NullValidationHandler} from './token-validation/null-validation-handler';
+import {createDefaultLogger, createDefaultStorage} from './factories';
+import {DefaultHashHandler, HashHandler} from './token-validation/hash-handler';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [],
-  exports: [],
-})
+            imports: [CommonModule],
+            declarations: [],
+            exports: [],
+          })
 export class OAuthModule {
   static forRoot(
     config: OAuthModuleConfig = null,
     validationHandlerClass = NullValidationHandler
   ): ModuleWithProviders<OAuthModule> {
+    const providers: Provider[] = [
+      OAuthService,
+      UrlHelperService,
+      {provide: OAuthLogger, useFactory: createDefaultLogger},
+      {provide: OAuthStorage, useFactory: createDefaultStorage},
+      {provide: ValidationHandler, useClass: validationHandlerClass},
+      {provide: HashHandler, useClass: DefaultHashHandler},
+      {
+        provide: OAuthResourceServerErrorHandler,
+        useClass: OAuthNoopResourceServerErrorHandler,
+      },
+      {provide: OAuthModuleConfig, useValue: config},
+      {provide: DateTimeProvider, useClass: SystemDateTimeProvider},
+    ];
+    if (!config?.noInterceptor) {
+      providers.push({
+                       provide: HTTP_INTERCEPTORS,
+                       useClass: DefaultOAuthInterceptor,
+                       multi: true,
+                     });
+    }
     return {
       ngModule: OAuthModule,
       providers: [
         OAuthService,
         UrlHelperService,
-        { provide: OAuthLogger, useFactory: createDefaultLogger },
-        { provide: OAuthStorage, useFactory: createDefaultStorage },
-        { provide: ValidationHandler, useClass: validationHandlerClass },
-        { provide: HashHandler, useClass: DefaultHashHandler },
+        {provide: OAuthLogger, useFactory: createDefaultLogger},
+        {provide: OAuthStorage, useFactory: createDefaultStorage},
+        {provide: ValidationHandler, useClass: validationHandlerClass},
+        {provide: HashHandler, useClass: DefaultHashHandler},
         {
           provide: OAuthResourceServerErrorHandler,
           useClass: OAuthNoopResourceServerErrorHandler,
         },
-        { provide: OAuthModuleConfig, useValue: config },
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: DefaultOAuthInterceptor,
-          multi: true,
-        },
-        { provide: DateTimeProvider, useClass: SystemDateTimeProvider },
+        {provide: OAuthModuleConfig, useValue: config},
+
+        {provide: DateTimeProvider, useClass: SystemDateTimeProvider},
       ],
     };
   }
